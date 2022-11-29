ecfea690a61b71e28d0b62271cd84ed3797f0273
dejenetesfaye <deju1921@gmail.com>
2022-11-29 07:32:00 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import MainPage from './comp/MainPage'
+import SideBar from './comp/SideBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+function App() {
+  return (
+    <div className='App'>
+      <SideBar />
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
---
+++ src/comp/SideBar.js
@@ -0,0 +1,39 @@
+import { React } from 'react'
+import myPhoto from '../img/myPhoto.jpg'
+
+export default function SideBar() {
+  function ScrollToABOUT(id) {
+    var element = document.getElementById(id)
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  return (
+    <div id='SideBar'>
+      <div class='image-container'>
+        <img id='myPhoto' src={myPhoto} alt='myPhoto' />
+      </div>
+      <div>
+        <div class='nav-item' onClick={() => ScrollToABOUT('ABOUT')}>
+          ABOUT
+        </div>
+        <div class='nav-item' onClick={() => ScrollToABOUT('EXPERIANCE')}>
+          EXPERIANCE
+        </div>
+        <div class='nav-item' onClick={() => ScrollToABOUT('EDUCATION')}>
+          EDUCATION
+        </div>
+        <div class='nav-item' onClick={() => ScrollToABOUT('SKILLS')}>
+          SKILLS
+        </div>
+        <div class='nav-item' onClick={() => ScrollToABOUT('INTERESTS')}>
+          INTERESTS
+        </div>
+        <div class='nav-item' onClick={() => ScrollToABOUT('AWARDS')}>
+          AWARDS
+        </div>
+      </div>
+    </div>
+  )
+}
